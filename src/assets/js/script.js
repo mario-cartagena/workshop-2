c0269bfee9831a1ae52f32b4da673890e7c98ed8
mario-cartagena <jhonmariocartagena@gmail.com>
2023-04-28 12:04:46 -0500

    prueba responsive

---
+++ src/assets/js/script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', main);
+
+var contador = 1;
+
+function main() {
+    var menuBar = document.querySelector('.header__menu-bar');
+    var nav = document.querySelector(".header__nav");
+
+    menuBar.addEventListener('click', function () {
+        alert("hola")
+        // if (contador === 1) {
+        //     nav.style.left = '0';
+        //     contador = 0;
+        // } else {
+        //     contador = 1;
+        //     nav.style.left = '-100%';
+        // }
+    });
+
+};
+
+
+
+let menuInternoOne = document.querySelector(".list__sub-items");
+let menuInternoTwo = document.querySelector(".list__sub-itemsTwo");
+
+function navbar() {
+    let btnFeature = document.getElementById("btn__feature");
+    btnFeature.addEventListener("click", () => {
+        menuInternoOne.classList.toggle("showItems")
+    })
+     let btnCompany = document.getElementById("btn__company");
+    btnCompany.addEventListener("click", () => {
+       menuInternoTwo.classList.toggle("showItems")
+    })
+}
+navbar()
